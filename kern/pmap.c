701de5edb3044656f36f6f51dca654f5bf0dd994
Deniss Paltovs <deniss.paltovs@gmail.com>
2016-10-12 19:30:15 +0300

    Fix UPAGES mapping according memory map specification

--- kern/pmap.c
+++ kern/pmap.c
@@ -169,8 +169,7 @@ mem_init(void)
 	//      (ie. perm = PTE_U | PTE_P)
 	//    - pages itself -- kernel RW, user NONE
 	// Your code goes here:
-    size_t pages_size = ROUNDUP(npages*sizeof(struct PageInfo), PGSIZE);
-    boot_map_region(kern_pgdir, UPAGES, pages_size, PADDR(pages), PTE_W);
+    boot_map_region(kern_pgdir, UPAGES, PTSIZE, PADDR(pages), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
